743e55f9faa1cf426ccab203fb15c507025d0afc
Trebor Huang <2300936257@qq.com>
2020-03-15 00:29:27 +0800

    CLEAN UP CODE STRUCTURE
    Factored out a method to render and send LaTeX.
    Cleared up some repetitive code.

--- cq.py
+++ cq.py
@@ -21,7 +21,9 @@ sleeping = False
 bot = CQHttp(api_root='http://192.168.56.101:5700/')
 application = bot.wsgi
 repeat_count = dict()
-latex_packages = ("bm", "array", "amsfonts", "amsmath", "amssymb", "mathtools", "tikz-cd", "mathrsfs", "xcolor")
+latex_packages = ("bm", "array", "amsfonts", "amsmath", "amssymb", "mathtools", "tikz-cd", "mathrsfs", "xcolor", "mathdots")
+
+# ------ Brain Power ------
 
 BP_raw = """Adrenaline is pumping
 Adrenaline is pumping
@@ -218,9 +220,7 @@ def brainpower(gid, inp):
                 else:
                     return ""
 
-def calc_cmd(event, comm):
-    res = parse_expr(comm[4:].strip(), transformations=standard_transformations + (implicit_multiplication_application,))
-    bot.send(event, str(res), auto_escape= True)
+# ------ END ------
 
 app = 0
 help_string = "所有命令以'> '开头。命令列表：\n" +\
@@ -232,164 +232,141 @@ help_string = "所有命令以'> '开头。命令列表：\n" +\
     "附加功能：\n   - 复读\n   - 脑力\n这些功能不是命令，不能用'> '触发，具体方法比较显然，请自己探索。"
 
 
+def render_latex_and_send(res, event):
+        try:
+            filename = "TEMP" + hex(hash(res) ^ time.time_ns())[1:]
+            latexify.latexify(res, filename="./img/" + filename, definitions="\\textwidth=7cm", usepackage=latex_packages)
+            # print("Sync", requests.get("http://192.168.56.101:5679/sync"))
+            try:
+                bot.delete_msg(message_id=event['message_id'])
+            except Exception:
+                print("Failed to recall latex spam.")
+            return {'reply': f"[CQ:image,file=file:///G:\\{filename}.jpeg]", 'auto_escape': False, 'at_sender': True}
+        except RuntimeError as e:
+            bot.send_private_msg(user_id=event['user_id'], message=event['message'])
+            bot.send_private_msg(user_id=event['user_id'], message = "错误如下：\n" + str(e).strip(), auto_escape=True)
+            try:
+                bot.delete_msg(message_id=event['message_id'])
+            except Exception:
+                print("Failed to recall latex spam.")
+            return {'reply': 'LaTeX有错，已发私聊'}
+
 @bot.on_message
 def handle_msg(event):
     global repeat_count, app, sleeping
-    if random.randint(1,100) == 1:
+    if random.randint(1,30) == 1:
         bot.clean_data_dir(data_dir="image")
     if event['message_type'] == "group":
-        #if "草" in event['message']:
-        #    return {'reply': """草(消歧义)\n\n你可能指：\n草(中文)\n草(日文)\n草(中日双语)\n草(英文)\n草(一种植物)\n草(Minecraft)\n草(两格高)\n草(化学式)\n草(鲁迅刻在桌上的字符)""", 'at_sender': False, 'auto_escape': True}
-        if sleeping:
-            if event['message'] == "> wake" and event['user_id'] == 2300936257:
-                sleeping = False
-                return {'reply': "来了qwq", "at_sender": "False"}
-            return
-        if (event['message'][-3:].lower() == 'dai' \
-          or (pinyin.get(''.join(filter(lambda c: '\u4e00' <= c <= '\u9fff', event['message'])), format="strip").strip("。，？（！…—；：“”‘’《》～·）()").strip())[-3:] == 'dai'):
-            if random.randint(1,2) == 2:
-                return {'reply': "Daisuke~", 'at_sender': False, 'auto_escape': True}
-        if (r := brainpower(event['group_id'], event['message'])) is not None:
-            repeat_count = dict()
-            app = (app + 1) % 4
-            print(bp_stage, regular_stage_number, r)
-            if bp_stage == 3:
-                print(next_in_lyrics(regular_stage_number))
-            if r == "":
+        try:
+            if sleeping:
+                if event['message'] == "> wake" and event['user_id'] == 2300936257:
+                    sleeping = False
+                    return {'reply': "来了qwq", "at_sender": "False"}
                 return
-            return {'reply': r + [" ", "（", "（（", "!"][app], 'at_sender': False, 'auto_escape': True}
-        if event['message'][0:2] == '> ':
-            # command mode
-            comm = event['message'][2:].replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", '&')
-            comms = comm.split()
-            c = comms[0].capitalize()
-            if c == 'Help':
-                cms = comm[4:].strip()
-                print("--- Help here. ---", cms)
-                if cms == '':
-                    bot.send_private_msg(message=help_string, user_id=event['user_id'], auto_escape=True)
-                    return {'reply': "帮助已发送至私聊"}
-                try:
+            if (event['message'][-3:].lower() == 'dai' \
+            or (pinyin.get(''.join(filter(lambda c: '\u4e00' <= c <= '\u9fff', event['message'])), format="strip").strip("。，？（！…—；：“”‘’《》～·）()").strip())[-3:] == 'dai'):
+                if random.randint(1,2) == 2:
+                    return {'reply': "Daisuke~", 'at_sender': False, 'auto_escape': True}
+            if (r := brainpower(event['group_id'], event['message'])) is not None:
+                repeat_count = dict()
+                app = (app + 1) % 4
+                print(bp_stage, regular_stage_number, r)
+                if bp_stage == 3:
+                    print(next_in_lyrics(regular_stage_number))
+                if r == "":
+                    return
+                return {'reply': r + [" ", "（", "（（", "!"][app], 'at_sender': False, 'auto_escape': True}
+            if event['message'][0:2] == '> ':
+                # command mode
+                comm = event['message'][2:].replace("&#91;", "[").replace("&#93;", "]").replace("&amp;", '&')
+                comms = comm.split()
+                c = comms[0].capitalize()
+                if c == 'Help':
+                    cms = comm[4:].strip()
+                    if cms == '':
+                        bot.send_private_msg(message=help_string, user_id=event['user_id'], auto_escape=True)
+                        return {'reply': "帮助已发送至私聊"}
                     if any(['\u4e00' <= c <= '\u9fff' for c in comm]):
                         return {'reply': "不支持汉字变量的计算。"}
                     res = parse_expr(comm[4:].strip())
                     if not res.__doc__:
                         return {'reply': "这个东西没有帮助文档诶"}
                     bot.send_private_msg(message=f"这个对象：\n\n{str(res)}\n\n的帮助文档如下：", user_id=event['user_id'], auto_escape=True)
-                    latexify.pdf_doc(comm[4:].strip(), res.__doc__)
                     bot.send_private_msg(message=clamp(res.__doc__, 2000), user_id=event['user_id'], auto_escape=True)
                     # TODO this is currently undoable since the image is probably too big
                     # TODO we'll send the help in pages instead
+                    #! latexify.pdf_doc(comm[4:].strip(), res.__doc__)
                     #! bot.send_private_msg(message=f"[CQ:image,file=file:///G:\\{comm[4:].strip()}.jpeg]", user_id=event['user_id'], auto_escape=False)
                     return {'reply': "帮助已发送至私聊", 'auto_escape': True}
-                except Exception as e:
-                    return {'reply': '报错了qaq: ' + str(e), 'at_sender': False, 'auto_escape': True}
-            if c == 'Echo' and event['user_id'] == 2300936257:
-                return {'reply': comm[4:].strip(), 'at_sender': False, 'auto_escape': True}
-            ## These are dangerous leaky operations, and only I can use it.
-            if c == 'Eval' and event['user_id'] == 2300936257:
-                try:
-                    res = eval(comm[4:].strip(), globals(), numpy.__dict__)
-                    return {'reply': str(res), 'at_sender': False, 'auto_escape': True}
-                except Exception as e:
-                    return {'reply': '报错了qaq: ' + str(e), 'at_sender': False, 'auto_escape': True}
-            if c == 'Calc':
-                try:
-                    if '^' in comm:
-                        bot.send(event, message="^是异或的符号，**是幂，你确定吗？")
-                    if any(['\u4e00' <= c <= '\u9fff' for c in comm]):
-                        return {'reply': "不支持汉字变量的计算。"}
-                    res = parse_expr(comm[4:].strip())#, transformations=standard_transformations + (implicit_multiplication_application,))
-                    return {'reply': clamp(str(res)), 'auto_escape': True}
-                except Exception as e:
-                    return {'reply': '报错了qaq: ' + str(e), 'at_sender': False, 'auto_escape': True}
-            
-            if c == 'Ord':
-                reply = requests.get("http://192.168.56.101:5679/ord", params={"cmd": comm[3:].strip()})
-                reply_text = reply.text.strip()
-                if len(reply_text) > 100:
-                    bot.send_private_msg(message=reply_text, auto_escape=True, user_id=event['user_id'])
-                    return {"reply": "有点太长了，已发私聊"}
-                return {"reply": reply.text.strip(), "auto_escape": True}
-            if c == 'Render':
-                try:
-                    res = comm[6:]
-                    filename = "RENDER" + hex(hash(res) ^ time.time_ns())[1:]
-                    latexify.latexify(res, filename="./img/" + filename, definitions="\\textwidth=7cm", usepackage=latex_packages)
-                    # print("Sync", requests.get("http://192.168.56.101:5679/sync"))
+                if c == 'Echo' and event['user_id'] == 2300936257:
+                    return {'reply': comm[4:].strip(), 'at_sender': False, 'auto_escape': True}
+                ## These are dangerous leaky operations, and only I can use it.
+                if c == 'Eval' and event['user_id'] == 2300936257:
                     try:
-                        bot.delete_msg(message_id=event['message_id'])
-                    except Exception:
-                        print("Failed to recall latex spam.")
-                    return {'reply': f"[CQ:image,file=file:///G:\\{filename}.jpeg]", 'auto_escape': False, 'at_sender': True}
-                except RuntimeError as e:
-                    bot.send_private_msg(user_id=event['user_id'], message=event['message'])
-                    bot.send_private_msg(user_id=event['user_id'], message = "错误如下：\n" + str(e).strip(), auto_escape=True)
+                        res = eval(comm[4:].strip(), globals(), numpy.__dict__)
+                        return {'reply': str(res), 'at_sender': False, 'auto_escape': True}
+                    except Exception as e:
+                        return {'reply': '报错了qaq: ' + str(e), 'at_sender': False, 'auto_escape': True}
+                if c == 'Calc':
                     try:
-                        bot.delete_msg(message_id=event['message_id'])
-                    except Exception:
-                        print("Failed to recall latex spam.")
-                    return {'reply': 'LaTeX有错，已发私聊'}
-                except Exception as e:
-                    return {'reply': '报错了qaq: ' + clamp(str(e)), 'at_sender': False, 'auto_escape': True}
-            if c == 'Latex':
-                try:
-                    res = comm[5:]
-                    filename = "LATEX" + hex(hash(res) ^ time.time_ns())[1:]
-                    #sympy.preview(f"$${res}$$", viewer='file', output='png', filename=f'./img/{hash(res)}.png', euler=False, dvioptions=["-T", "tight", "-z", "0", "--truecolor", "-D 500"], packages=latex_packages)
-                    latexify.latexify(f"$\\displaystyle {res}$", filename='./img/' + filename, usepackage=latex_packages)
-                    try:
-                        bot.delete_msg(message_id=event['message_id'])
-                    except Exception:
-                        print("Failed to recall latex spam.")
-                    return {'reply': f"[CQ:image,file=file:///G:\\{filename}.jpeg]", 'auto_escape': False, 'at_sender': True}
-                except RuntimeError as e:
-                    bot.send_private_msg(user_id=event['user_id'], message=event['message'])
-                    bot.send_private_msg(user_id=event['user_id'], message = "错误如下：\n" + str(e).strip(), auto_escape=True)
-                    try:
-                        bot.delete_msg(message_id=event['message_id'])
-                    except Exception:
-                        print("Failed to recall latex spam.")
-                    return {'reply': 'LaTeX有错，已发私聊'}
-                except Exception as e:
-                    return {'reply': '报错了qaq: ' + clamp(str(e)), 'at_sender': False, 'auto_escape': True}
-            if c == 'Sleep' and event['user_id'] == 2300936257:
-                sleeping = True
-                return {'reply': 'zzz', 'at_sender': False}
-            return {'reply': "憨批（试下 > help", 'auto_escape': True}
-        if event['group_id'] in repeat_count:
-            # count the number of last repeat
-            if repeat_count[event['group_id']][0] == str(event['raw_message']):
-                repeat_count[event['group_id']][1] += 1
-            else:
-                repeat_count[event['group_id']][0] = str(event['raw_message'])
-                if repeat_count[event['group_id']][1] > 1:
+                        if '^' in comm:
+                            bot.send(event, message="^是异或的符号，**是幂，你确定吗？")
+                        if any(['\u4e00' <= c <= '\u9fff' for c in comm]):
+                            return {'reply': "不支持汉字变量的计算。"}
+                        res = parse_expr(comm[4:].strip())#, transformations=standard_transformations + (implicit_multiplication_application,))
+                        return {'reply': clamp(str(res)), 'auto_escape': True}
+                    except Exception as e:
+                        return {'reply': '报错了qaq: ' + str(e), 'at_sender': False, 'auto_escape': True}
+                if c == 'Ord':
+                    reply = requests.get("http://192.168.56.101:5679/ord", params={"cmd": comm[3:].strip()})
+                    reply_text = reply.text.strip()
+                    if len(reply_text) > 100:
+                        bot.send_private_msg(message=reply_text, auto_escape=True, user_id=event['user_id'])
+                        return {"reply": "有点太长了，已发私聊"}
+                    return {"reply": reply.text.strip(), "auto_escape": True}
+                if c == 'Render':
+                    return render_latex_and_send(comm[6:].strip(), event)
+                if c == 'Latex':
+                    return render_latex_and_send(f"$\\displaystyle {comm[5:].srtip()}$", event)
+                if c == 'Sleep' and event['user_id'] == 2300936257:
+                    sleeping = True
+                    return {'reply': 'zzz', 'at_sender': False}
+                return {'reply': "憨批（试下 > help", 'auto_escape': True}
+            if event['group_id'] in repeat_count:
+                # count the number of last repeat
+                if repeat_count[event['group_id']][0] == str(event['raw_message']):
+                    repeat_count[event['group_id']][1] += 1
+                else:
+                    repeat_count[event['group_id']][0] = str(event['raw_message'])
+                    if repeat_count[event['group_id']][1] > 1:
+                        repeat_count[event['group_id']][1] = 0
+                        if random.randint(1, 5) == 2:
+                            return {'reply': "打断复读的事屑（确信", 'at_sender': False, 'auto_escape': True}
+                        if random.randint(1, 7) == 6:
+                            return {'reply': "？？", 'at_sender': False, 'auto_escape': True}
+                    repeat_count[event['group_id']][1] = 1
+                if repeat_count[event['group_id']][1] >= 7:
                     repeat_count[event['group_id']][1] = 0
-                    if random.randint(1, 5) == 2:
-                        return {'reply': "打断复读的事屑（确信", 'at_sender': False, 'auto_escape': True}
-                    if random.randint(1, 7) == 6:
-                        return {'reply': "？？", 'at_sender': False, 'auto_escape': True}
-                repeat_count[event['group_id']][1] = 1
-            if repeat_count[event['group_id']][1] >= 7:
-                repeat_count[event['group_id']][1] = 0
-                return {'reply': "适度复读活跃气氛，过度复读影响交流。为了您和他人的健康，请勿过量复读。", 'at_sender': False, 'auto_escape': True}
-        else:
-            repeat_count[event['group_id']] = [str(event['raw_message']), 1]
-        if random.randint(1, repeat_count[event['group_id']][1]+1) >= 3:
-            repeat_count[event['group_id']][1] = 0 # prevent spamming
-            if random.randint(1, 30) == 4:
-                return {'reply': "复  读  大  失  败", 'at_sender': False, 'auto_escape': True}
-            if random.randint(1, 10) == 1:
-                return {'reply': "（", 'at_sender': False, 'auto_escape': True}
-            if random.randint(1, 20) == 33:
-                return {'reply': "打断（（", 'at_sender': False, 'auto_escape': True}
-            return {'reply': event['raw_message'], 'at_sender': False, 'auto_escape': False}
-        if random.randint(1, 455) == 44 and event['group_id'] == 730695976:
-            return {'reply': "3倍ice cream☆☆！！！", 'at_sender': False, 'auto_escape': True}
-        if random.randint(1, 360) == 144 and event['group_id'] == 730695976:
-            return {'reply': "爬", 'at_sender': False, 'auto_escape': True}
-        if random.randint(1, 1000) == 111 and event['group_id'] == 80852074:
-            return {'reply': "最喜欢qlbf了（", "at_sender": False, 'auto_escape': True}
+                    return {'reply': "适度复读活跃气氛，过度复读影响交流。为了您和他人的健康，请勿过量复读。", 'at_sender': False, 'auto_escape': True}
+            else:
+                repeat_count[event['group_id']] = [str(event['raw_message']), 1]
+            if random.randint(1, repeat_count[event['group_id']][1]+1) >= 3:
+                repeat_count[event['group_id']][1] = 0 # prevent spamming
+                if random.randint(1, 30) == 4:
+                    return {'reply': "复  读  大  失  败", 'at_sender': False, 'auto_escape': True}
+                if random.randint(1, 10) == 1:
+                    return {'reply': "（", 'at_sender': False, 'auto_escape': True}
+                if random.randint(1, 20) == 33:
+                    return {'reply': "打断（（", 'at_sender': False, 'auto_escape': True}
+                return {'reply': event['raw_message'], 'at_sender': False, 'auto_escape': False}
+            if random.randint(1, 455) == 44 and event['group_id'] == 730695976:
+                return {'reply': "3倍ice cream☆☆！！！", 'at_sender': False, 'auto_escape': True}
+            if random.randint(1, 360) == 144 and event['group_id'] == 730695976:
+                return {'reply': "爬", 'at_sender': False, 'auto_escape': True}
+            if random.randint(1, 1000) == 111 and event['group_id'] == 80852074:
+                return {'reply': "最喜欢qlbf了（", "at_sender": False, 'auto_escape': True}
+        except Exception as e:
+            return {'reply': '报错了qaq: ' + clamp(str(e)), 'at_sender': False, 'auto_escape': True}
     elif event['message_type'] == "private":
         bot.send_private_msg(message=help_string, user_id=event['user_id'], auto_escape=True)
         return {'reply': "Bot几乎只有群聊功能"}
@@ -400,8 +377,6 @@ def handle_group_increase(event):
     print(event)
     if event['group_id'] == 80852074 and event['sub_type'] == "invite" and event['operator_id'] == 1289817086:
         bot.send(event, message='神音姐姐又拉人了', auto_escape=True)
-    if random.randint(1, 30) == 16:
-        bot.send(event, message='其实每次有个人加群就欢迎是很憨的，但是这段代码放在这里可以提醒我如何在有人加群的时候触发动作，所以我才没有删掉（', auto_escape=True)
     return
 
 @bot.on_request('group', 'friend')
